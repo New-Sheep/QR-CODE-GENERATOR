bcfaae34e238244c9163152fd820f44a63b29dd2
Ankit <ankityiitr@gmail.com>
2023-08-11 16:17:53 +0530

    Add /generateQR endpoint

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const qrcode = require('qrcode');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use(express.json());
+
+app.post('/generateQR', async (req, res) => {
+    // Implementation for generating QR codes
 });
 
